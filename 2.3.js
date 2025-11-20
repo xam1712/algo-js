8732505156dbd2446a0f306e29ef957ee2cc9a67
Kevin Verbaert <kev.verbaert@gmail.com>
2025-11-20 14:18:19 +0100

    exo 2.3.avant relance du terminal

--- 2.3.js
+++ 2.3.js
@@ -1,6 +1,9 @@
 let i = 1;
 
-while (i <= 100) {
-    console.log(i);
-    i = i + 1;
-}
+//while (i <= 100) {
+  //  console.log(i);
+    //alert(i);
+   // i++;
+//}
+
+for (let i = 1; i <= 100; i++)
