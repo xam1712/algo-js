746db2ce639186cb38756a2792d7d044bc98ed72
Kevin Verbaert <kev.verbaert@gmail.com>
2025-11-20 14:51:13 +0100

    exo 2.4

---
+++ 2.4.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i + "pair" + (i / 2));
+        alert(i + " pair " + (i / 2));
+
+    } else {
+        console.log(i + " impair " + (i / 3));
+        alert(i + " impair " + (i / 3));
+    }
+}
