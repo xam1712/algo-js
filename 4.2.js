3e75525d8f39a36975f53e247bd69ce48011ea44
Kevin Verbaert <kev.verbaert@gmail.com>
2025-11-24 14:28:20 +0100

    exo 4.2+commentaires

--- 4.2.js
+++ 4.2.js
@@ -1,8 +1,8 @@
 
-let max = 10;
+let max = 10; // Définir la valeur maximale
 
-function rand10() {
-    return Math.floor(Math.random() * max) + 1;
+function rand10() { // fonction pour générer un nombre aléatoire entre 1 et 10
+    return Math.floor(Math.random() * max) + 1; //générer un nombre aléatoire compris dans la valeur définie
 }
 
 console.log(rand10()); // renvoie un nombre entier compris entre 1 et 10
