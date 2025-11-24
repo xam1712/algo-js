88502555a51c58216b687491aed1c617c6a7d7f5
Kevin Verbaert <kev.verbaert@gmail.com>
2025-11-24 10:17:52 +0100

    exo 3.0

---
+++ 3.0.js
@@ -0,0 +1,22 @@
+
+let arr = ["thelichekinge",
+  "ethan",
+  "Hetaska",
+  "Enrique2802",
+  "Trollman341",
+  "Kisame25021990",
+  "Léandre Mpore-Ghozzo",
+  "Abdoul",
+  "Aucouturier Jason",
+  "xam1712",
+  "Dodo-31",
+  "MattWare",
+  "Thomas",
+  "lalbecade",
+  "Gaetan",
+  "Dimitri Gillain",
+  "TyberCusa","Johan"];
+
+for (let elem of arr) {
+    console.log("Liste des participants : " + elem);
+}
